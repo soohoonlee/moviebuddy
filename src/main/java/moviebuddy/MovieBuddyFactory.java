fb482cd5bceda3e26f0a349b9e150c9cca1eed03
soohoonlee <jjinimania@gmail.com>
2022-01-25 06:19:34 +0900

    section_15: Add metadata caching

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -1,11 +1,16 @@
 package moviebuddy;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @PropertySource("/application.properties")
@@ -19,6 +24,14 @@ public class MovieBuddyFactory {
         marshaller.setPackagesToScan("moviebuddy");
 
         return marshaller;
+    }
+
+    @Bean
+    public CacheManager caffeineCacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder().expireAfterWrite(3, TimeUnit.SECONDS));
+
+        return cacheManager;
     }
 
     @Configuration
